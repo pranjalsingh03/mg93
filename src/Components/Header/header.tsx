0c7a4b55a791f2264147a8fbe2818beb8d217a1c
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-07-20 23:33:41 +0530

    feat: Add Moon and Sun icons to header for dark mode toggle

---
+++ src/Components/Header/header.tsx
@@ -0,0 +1,53 @@
+"use client";
+import React, { FC } from 'react';
+import './header.css';
+import logo from "../../Images/logo1.png"
+import { useState, useEffect } from "react";
+import Sun from "../../../public/Sun.svg";
+import Moon from "../../../public/Moon.svg";
+
+
+const Header: FC=()=> {
+
+  const [theme ,setTheme]= useState("dark-theme");
+  const togglethemes = () =>{
+    if(theme === "dark-theme"){
+      setTheme("light-theme");
+    }else{
+      setTheme("dark-theme");
+    }
+  };
+
+  useEffect(()=>{
+    document.body.className=theme;
+  },[theme]);
+  return (
+<>
+    <nav className="navbar navbar-expand-lg navbar-light">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="https://github.com/pranjalsingh03">
+        <img src={logo.toString()} alt='logo' className='image-logo-header' />
+      </a>
+      <div className='dark_mode'>
+        <input
+          className='dark_mode_input'
+          type='checkbox'
+          id='darkmode-toggle'
+          onClick={() => togglethemes()}
+        />
+        <label className='dark_mode_label' htmlFor='darkmode-toggle'>
+          <Moon /><Sun />
+        </label>
+      </div>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <button className="button contactus-button">
+          <a className="nav-a" href="/">Resume</a>
+        </button>
+      </div>
+    </div>
+  </nav>
+    </>
+  );
+}
+
+export default Header
