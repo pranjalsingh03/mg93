5641b9f29e96b1ea3f44e34323a1f32c367b341d
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-07-20 23:33:56 +0530

    chore: Update next.config to enable SVG loading and disable fs fallback

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.fallback.fs = false;
+        }
+
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+
+        return config;
+    },
+};
 
 export default nextConfig;
