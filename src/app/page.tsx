40b85a63fa5ddae93644b669ab1f775af9cd0631
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-07-22 23:35:05 +0530

    chore: Update npm dependencies and add Bootstrap and Framer Motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Intro from "../Components/Intro/intro";
+import Header from "../Components/Header/header";
+import HomeM from "../Components/Home/home"
 import "../app/globals.css"
+import Lang from "@/Components/Languages/Lang";
 
 export default function Home() {
   return (
@@ -9,7 +12,10 @@ export default function Home() {
         <div id="stars" />
         <div id="stars2" />
         <div id="stars3" />
+      <Header/>
       <Intro/>  </div>
+      <HomeM/>
+      <Lang/>
     </>
   );
-}
+}2
