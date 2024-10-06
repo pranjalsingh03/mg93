67f82137c52821d7d05ae1d512a4c4486cde8441
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-10-07 00:42:45 +0530

    Refactor file paths for logo and profile images

--- src/Components/Header/header.tsx
+++ src/Components/Header/header.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { FC } from 'react';
 import './header.css';
-import logo from "../../app/logo1.png"
+import logo from "../../../public/logo1.png"
 import { useState, useEffect } from "react";
 import Sun from "../../app/Sun.svg";
 import Moon from "../../app/Moon.svg";
--- src/Components/Home/home.tsx
+++ src/Components/Home/home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./home.css"
 import Image from "next/image";
-import profile from "../../app/profile.jpg";
+import profile from "../../../public/profile.jpg";
 
 const HomeM = () => {
     return (
