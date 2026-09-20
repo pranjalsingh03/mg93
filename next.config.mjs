9a799b4e33c83335bc9a6c2eb56e6414ceb87233
Pranjal-singh <pranjalsingh9304@gmail.com>
2026-09-20 12:16:51 +0530

    Add distDir: 'dist' to next.config.mjs to match Vercel output directory setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    distDir: 'dist',
     webpack: (config, { isServer }) => {
         if (!isServer) {
             config.resolve.fallback.fs = false;
